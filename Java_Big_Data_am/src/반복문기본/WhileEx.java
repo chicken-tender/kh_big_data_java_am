4779e04edefc68efc48380bfa6914533850d245a
ygm815 <ygm815@naver.com>
2023-01-04 15:01:18 +0900

    반복문 연습 : 나무찍기(주석추가)

--- Java_Big_Data_am/src/반복문기본/WhileEx.java
+++ Java_Big_Data_am/src/반복문기본/WhileEx.java
@@ -1,5 +1,6 @@
 package 반복문기본;
-
+// 사용자에게 입력을 받을 때 잘못 입력한 경우 무한루프로 돌려야 함. 그럴 때는 while
+// 반복해야 하는 범위가 명확하면 for문으로 작성
 public class WhileEx {
     public static void main(String[] args) {
         int treeHit = 0;
