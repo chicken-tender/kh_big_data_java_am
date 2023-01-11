2cd21f5b11e47605e574a92970b9404dbd7e865f
ygm815 <ygm815@naver.com>
2023-01-11 15:05:19 +0900

    유형 추가

--- src/메소드연습0110/ClassMethod.java
+++ src/메소드연습0110/ClassMethod.java
@@ -4,16 +4,25 @@ import java.util.Scanner;
 // 기본 형태 : 접근제한자 반환타입 메소드명(매개변수 목록) { 구현부; return }
 public class ClassMethod {
     public static void main(String[] args) {
-        Sample sample = new Sample(); // 클래스 내에 생성자가 없으면 기본 생성자가 호출됨!!
+        // 생성자 호출시 전달하는 매개변수가 없음
+        // 클래스 내에 생성자가 없으면 기본 생성자가 호출됨!!
+        Sample sample = new Sample();
+
         Scanner sc = new Scanner(System.in);
         System.out.print("정수값 입력 : ");
         int n1 = sc.nextInt();
         System.out.print("정수값 입력 : ");
         int n2 = sc.nextInt();
 
-        int rst = sample.sum(n1, n2); // sum()은 매개변수와 반환값이 있는 메소드
+        int rst = sample.sum(n1, n2); // sum()은 메소드 호출부에서 인수로 값을 전달
         System.out.println(rst);
+
+        // say() 메소드를 호출하기 위해서는 클래스를 객체로 만들고 객체의 메소드로 접근
         sample.say(); // System.out.println은 반환값이 있어야 출력 가능! 반환값 없으면 메소드만 써도 됨.
-        System.out.println(sample.sayStr()); // return 값을 String으로 반환하기 때문에 System.out.print 출력 가능
+
+        System.out.println(sample.sayStr()); // return 값을 String 으로 반환하기 때문에 System.out.print 출력 가능
+
+        int rst2 = sample.sum(1,2,3,4,5);
+        System.out.println(rst2);
     }
 }
--- src/메소드연습0110/Sample.java
+++ src/메소드연습0110/Sample.java
@@ -1,14 +1,41 @@
 package 메소드연습0110;
 
 public class Sample {
+    // 생성자 생성시 인스턴스 필드에 값이 들어감.(초기화값)
+    String name; // 인스턴스(필드) - 객체가 만들어질 때 함께 만들어지는 값
+
+    // 생성자 : 클래스 이름과 동일하고 반환 타입이 없음, 클래스가 객체로 생성될 때 자동으로 호출
+    // 생성자의 주요 목적 : 클래스 내부의 인스턴스 필드를 초기화
+    Sample() {
+        name = "anonymous";
+    }
+    // 동일 이름의 생성자가 여러개 만들어지면 생성자 오버로딩 관계가 만들어 진다.
+    Sample(String name) {
+        this.name = name; // this 는 추후에 설명...
+    }
+
+    // 이름을 읽는 메소드 (Getter)
+    String getName() {
+        return name;
+    }
+
+    // *** 동일 이름의 함수가 여러개 만들어지면 함수 오버로딩 관계가 만들어 진다.
     // 접근제한자가 없으면 default 특성을 가짐..-> 동일 패키지 내에서만 접근 가능
     int sum(int a, int b) { // 메소드 호출시 매개변수로 값을 전달함!!!
         return a + b; // 메소드 호출 결과를 정수값으로 반환!!!
     }
+    // 메소드의 매개변수는 일반적으로 갯수가 정해져 있으나 경우에 따라 갯수를 알 수 없는 경우 ... 을 사용하면 됨.
+    int sum(int ... values) {
+        int sum = 0;
+        for(int i = 0; i < values.length; i++) {
+            sum += values[i];
+        }
+        return sum;
+    }
 
     // 매개변수도 없고 반환값도 없는 메소드
     void say() {
-        System.out.print("안녕하세요");
+        System.out.println(getName() + "안녕하세요");
     }
 
     String sayStr() {
