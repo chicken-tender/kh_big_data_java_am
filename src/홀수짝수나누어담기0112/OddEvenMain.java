809b1a0f9e0dfb41ff6e721cdb013050019db6ae
ygm815 <ygm815@naver.com>
2023-01-13 19:02:16 +0900

    줄 정리

--- src/홀수짝수나누어담기0112/OddEvenMain.java
+++ src/홀수짝수나누어담기0112/OddEvenMain.java
@@ -2,12 +2,10 @@ package 홀수짝수나누어담기0112;
 
 public class OddEvenMain {
     public static void main(String[] args) {
-
         OddEvenSeparator ex = new OddEvenSeparator();
         ex.inputArray();
         ex.separatorArray();
         ex.printArray();
 
     }
-
 }
