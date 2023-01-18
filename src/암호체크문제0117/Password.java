f76905c027a9edc59f97e9e32b00c6cf8be3961c
ygm815 <ygm815@naver.com>
2023-01-18 18:43:32 +0900

    문제 주석 삭제

--- src/암호체크문제0117/Password.java
+++ src/암호체크문제0117/Password.java
@@ -1,10 +1,6 @@
 package 암호체크문제0117;
 import java.util.Scanner;
-// 암호의 길이는 10 ~ 30자 사이 (10 <= len <= 30)
-// 암호에는 숫자, 소문자, 대문자, 특수 문자가 포함되어야 함
-// 특수문자는 (!, %, _, #, &, +, -, *, /)의 9개 중 하나
-// 입력 받은 패스워드가 조건을 만족하면 "Good password", 아니면 "Bad password" 출력
-// 반복문을 사용하고 사용자가 "종료" 또는 "exit" 입력하면 프로그램 종료
+
 public class Password {
     String password;
 
