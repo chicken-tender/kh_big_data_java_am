3636b96058fde62b7335e4ba9bb2785a7c9f884d
ygm815 <ygm815@naver.com>
2023-01-20 09:17:41 +0900

    인터페이스 객체 생성(익명의 객체)하는 법 추가

--- src/인터페이스리모컨0119/RemoConMain.java
+++ src/인터페이스리모컨0119/RemoConMain.java
@@ -22,5 +22,39 @@ public class RemoConMain {
             rc.setMute(false);
         }
         RemoteControl.changeBattery(); // 인터페이스 소속의 메소드
+
+        // 인터페이스는 자기 자신의 객체를 생성 못하지만 일회용으로 쓰고 버릴 수 있음.
+        // 자기 자신 객체(?) 생성하고 아래 구현부를 만들면 가능 <- 익명의 객체임 이름 없음.
+        RemoteControl remoteControl = new RemoteControl() {
+            int volume;
+            @Override
+            public void turnOn() {
+                System.out.println("플스5를 켭니다.");
+            }
+
+            @Override
+            public void turnOff() {
+                System.out.println("플스5를 끕니다.");
+            }
+
+            @Override
+            public void setVol(int volume) {
+                if(volume > RemoteControl.MAX_VOL) {
+                    this.volume = RemoteControl.MAX_VOL;
+                } else if(volume < RemoteControl.MIN_VOL) {
+                    this.volume = RemoteControl.MIN_VOL;
+                } else {
+                    this.volume = volume;
+                }
+                System.out.println("현재 Audio 볼륨 : " + this.volume);
+            }
+
+            @Override
+            public void getInfo() {
+
+            }
+        }; // 끝났다는거 세미콜론 붙여서 알려줘야 함.
+        remoteControl.turnOn();
+        remoteControl.turnOff();
     }
 }
